9cc3aefa95fe9d2d82d8bdb8365e4245686689b3
techspiriteagle <techspiriteagle@gmail.com>
2019-01-12 12:53:53 +0530

    multi animation

--- app/src/main/java/com/spirit/tech/animation/MainActivity.java
+++ app/src/main/java/com/spirit/tech/animation/MainActivity.java
@@ -6,10 +6,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity  {
 
-    TextView click,tvhi;
+    TextView click,tvhi,tvandroid;
     boolean flag;
+    Button btnUi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,27 +19,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         click = findViewById(R.id.btnclick);
         tvhi = findViewById(R.id.newword);
+        tvandroid=findViewById(R.id.tvandroid);
+        btnUi=findViewById(R.id.btnUi);
 
-        click.setOnClickListener(this);
+        btnUi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                click.animate().scaleX(1f).rotation(14400).setDuration(2000);
+                tvhi.animate().alpha(1).translationY(0).rotation(-3600).setDuration(20000);
+                tvandroid.animate().translationY(0).scaleX(0.5f).scaleY(0.5f).setDuration(5000);
 
-    }
 
-    @Override
-    public void onClick(View v) {
-        if(v.getId()==R.id.btnclick)
-        {
-            if (!flag) {
-                flag = true;
-
-                click.animate().alpha(0).setDuration(3000);
-                tvhi.animate().alpha(1).setDuration(3000);
-            } else {
-                flag = false;
-                click.animate().alpha(1).setDuration(3000);
-                tvhi.animate().alpha(0).setDuration(3000);
 
             }
-        }
+        });
+
+//        click.setOnClickListener(this);
+
     }
+
+//    @Override
+//    public void onClick(View v) {
+//        if(v.getId()==R.id.btnclick)
+//        {
+//            if (!flag) {
+//                flag = true;
+//
+//                click.animate().alpha(0).setDuration(3000);
+//                tvhi.animate().alpha(1).setDuration(3000);
+//            } else {
+//                flag = false;
+//                click.animate().alpha(1).setDuration(3000);
+//                tvhi.animate().alpha(0).setDuration(3000);
+//
+//            }
+//        }
+//    }
 }
 
